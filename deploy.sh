2f529a501e84650294b1d91b501e033f96f33b6e
Henrik Kniberg <h@kniberg.com>
2018-02-04 17:24:42 +0100

    Added FAQ link

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ../output/translateme.tar.gz
-meteor build ../output --architecture os.linux.x86_64
-cd ../output
-mv translateme.tar.gz translateme.tar.gz
-echo "in nodechef, type deploy -i translateme"
-nodechef
